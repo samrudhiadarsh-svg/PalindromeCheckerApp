1ceba9f454c0ce38b02c2477e09c525d77418f44
Samrudhi <samrudhiadarsh@gmail.com>
2026-02-23 10:17:20 +0530

    [Samrudhi] Added UC1: Application Entry & Welcome Message

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,16 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        System.out.println("Welcome to Palindrome Checker App Management System");
-    }
-}
+
+
+
+
+                System.out.println("      Welcome to Palindrome Checker Application");
+                System.out.println("              Version 1.0");
+                System.out.println("This application checks whether a word or phrase is a palindrome.");
+                System.out.println("Program initialized successfully.");
+                System.out.println("Proceeding to next use case...");
+            }
+        }
+
+
+
